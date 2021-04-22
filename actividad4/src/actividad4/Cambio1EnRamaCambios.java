6a2202f791c27320dfec12208b6dfb6226dd2f5d
ronny <ronny@LAPTOP-80MR8PMB>
2021-04-22 18:39:44 +0200

    Cambio 1 en rama cambios

---
+++ actividad4/src/actividad4/Cambio1EnRamaCambios.java
@@ -0,0 +1,10 @@
+package actividad4;
+
+public class Cambio1EnRamaCambios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola 1 desde la rama cambios");
+	}
+
+}
