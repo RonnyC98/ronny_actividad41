6c39059b86110fffc604001be474f9853ea6aa86
ronny <ronny@LAPTOP-80MR8PMB>
2021-04-22 18:21:54 +0200

    Mi commit 1

---
+++ actividad4/src/actividad4/Metodos.java
@@ -0,0 +1,11 @@
+package actividad4;
+
+public class Metodos {
+	public static void main(String[] args) {
+		Saludo1();
+	}
+	
+	public static void Saludo1() {
+		System.out.println("Hola 1");
+	}
+}
